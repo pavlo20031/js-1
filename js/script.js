1e9ffb71b0829e6bfe03023a3101225c6876fe17
pavlo20031 <lesivpavlo999@gmail.com>
2025-12-31 21:45:58 +0200

    commit123

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// age
+const age = 12;
+console.log(age);
+
+//name
+const name = "Pavlo";
+console.log(name);
+
+//isStudent
+const isStudent = true;
+console.log(isStudent);
+
+//myString
+const myString = "Успіх – це здатність крокувати від однієї невдачі до іншої, не втрачаючи ентузіазму.";
+console.log(myString);
+
+//myNumber
+let myNumber = 45;
+myNumber += 10;
+console.log(myNumber);
+
+//myNull
+const myNull = null;
+console.log(myNull);
+
+//userName
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+alert(typeof userName);
+
+//userAnswer
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+//move
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
